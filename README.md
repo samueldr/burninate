5db3a4d5485df8e5a76ba9bdad00343b39a6d9ca
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-06-10 18:29:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+burninate
+=========
+
+Small shim to reduce headaches when writing disk images.
+
+This is written *for my own use*, so it may not fit your use.
+
+Limitations
+-----------
+
+By design, this will not write to a normal file, but only to block devices.
+
+By design, it uses `dd` under `pv`, as I haven't found a way to make `pv` use
+the equivalent of `oflag=direct,sync`.
---
+++ default.nix
@@ -0,0 +1,31 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+
+let
+  inherit (pkgs) ruby runCommandNoCC runtimeShell;
+  inherit (pkgs.lib) makeBinPath;
+  runtimePath = makeBinPath (with pkgs;[
+    coreutils
+    file
+    pv
+  ]);
+in
+
+# Build a wrapper-less version of the script.
+# The shebang and PATH are set at the top of the script.
+runCommandNoCC "burninate" {
+  inherit runtimePath;
+} ''
+  mkdir -p $out/bin
+
+  cat <<EOF > $out/bin/burninate
+  #!${ruby}/bin/ruby
+  ENV["PATH"] = "$runtimePath"
+
+  EOF
+
+  cat ${./burninate} >> $out/bin/burninate
+
+  chmod +x  $out/bin/burninate
+''
