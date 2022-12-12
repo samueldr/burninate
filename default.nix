4cb5850b65b37499fe50248386fb27f927bbfd57
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-12-11 20:35:21 -0500

    Update expression to make script hermetic

--- default.nix
+++ default.nix
@@ -1,32 +1,49 @@
-{
-  pkgs ? import <nixpkgs> {}
-}:
+{ pkgs ? import <nixpkgs> {} }:
 
-let
-  inherit (pkgs) ruby runCommandNoCC runtimeShell;
-  inherit (pkgs.lib) makeBinPath;
-  runtimePath = makeBinPath (with pkgs;[
-    coreutils
-    file
-    pv
-    zstd
-  ]);
-in
+pkgs.callPackage (
+
+{ lib
+, runCommand
+, runtimeShell
+, ruby
+, file
+, pv
+, coreutils
+, gzip
+, bzip2
+, xz
+, zstd
+}:
 
 # Build a wrapper-less version of the script.
 # The shebang and PATH are set at the top of the script.
-runCommandNoCC "burninate" {
-  inherit runtimePath;
-} ''
+runCommand "burninate" {} ''
   mkdir -p $out/bin
 
-  cat <<EOF > $out/bin/burninate
+  (cd $out/bin
+
+  cat <<EOF > burninate
   #!${ruby}/bin/ruby
-  ENV["PATH"] = "$runtimePath"
-
   EOF
+  cat ${./burninate} >> burninate
 
-  cat ${./burninate} >> $out/bin/burninate
+  substituteInPlace burninate \
+    --replace 'EXE_FILE = "file"' 'EXE_FILE = "${file}/bin/file"' \
+    --replace 'EXE_ZCAT = "zcat"' 'EXE_ZCAT = "${bzip2}/bin/zcat"' \
+    --replace 'EXE_XZCAT = "xzcat"' 'EXE_XZCAT = "${xz}/bin/xzcat"' \
+    --replace 'EXE_BZCAT = "bzcat"' 'EXE_BZCAT = "${bzip2}/bin/bzcat"' \
+    --replace 'EXE_ZSTDCAT = "zstdcat"' 'EXE_ZSTDCAT = "${zstd}/bin/zstdcat"' \
+    --replace 'EXE_PV = "pv"' 'EXE_PV = "${pv}/bin/pv"' \
+    --replace 'EXE_DD = "dd"' 'EXE_DD = "${coreutils}/bin/dd"'
 
-  chmod +x  $out/bin/burninate
+  echo ":: Checking for unmet dependencies..."
+  if grep -v '${builtins.storeDir}' burninate | grep '^EXE_'; then
+    echo "... found unmet dependencies, aborting!"
+    exit 1
+  fi
+
+  chmod +x  burninate
+  )
 ''
+
+) {}
